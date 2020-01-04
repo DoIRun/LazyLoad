5db123c752d2a6a24cb3b3e8698ded0eddb25624
Kevin Bodwell <kevin.bodwell@gmail.com>
2020-01-04 13:49:02 -0800

    Initial Commit

---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'DoIRun_LazyLoad',
+    __DIR__
+);
---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+            lazy: 'DoIRun_LazyLoad/js/jquery.lazy.min',
+            youtube: 'DoIRun_LazyLoad/js/jquery.lazy.youtube.min',
+        }
+    }
+}
